57f1ec7e4148a33d0f31444441b3494a158afe7b
CharlesChi715 <qiyaofu715@gmail.com>
2026-06-06 11:30:03 +1000

    modified: add OpenAI client and multi-round refinement loop

--- src/personal_learning_agent/__init__.py
+++ src/personal_learning_agent/__init__.py
@@ -1,2 +1,27 @@
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+from pprint import pprint as pp
+
+load_dotenv()
+
 def main() -> None:
-    print("Hello from personal-learning-agent!")
+    client = OpenAI(
+        # This is the default and can be omitted
+        api_key=os.environ.get("OPENAI_API_KEY"),
+    )
+    # score = 0
+    rounds = 0
+    instr = "Make this greeting more cheerful:"
+    starter = "Greeting in a cheerful tone."
+    # while score < 10 and rounds < 5:
+    while rounds < 5:
+        response = client.responses.create(
+            model="gpt-4o-mini",
+            instructions='' if rounds == 0 else instr,
+            input=starter if rounds == 0 else response.output_text,
+        )
+        
+        rounds += 1
+        print('response in round ' + str(rounds) + ':', response.output_text)
+    
